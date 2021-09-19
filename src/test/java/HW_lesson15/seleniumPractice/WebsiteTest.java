9bd6ec31e1ac0bb4e4114d3f79627db965e9c33b
samilyk <qareatestqarea@gmail.com>
2021-09-19 23:23:59 +0300

    Selenium Practice

--- src/test/java/HW_lesson15/seleniumPractice/WebsiteTest.java
+++ src/test/java/HW_lesson15/seleniumPractice/WebsiteTest.java
@@ -12,68 +12,68 @@ public class WebsiteTest extends BaseTest {
     @Test
     public void removeAllRecords() {
         webDriver.get("https://www.globalsqa.com/angularJs-protractor/BankingProject/#/login");
-        WebElement customerLoginButton = webDriver.findElement(By.xpath("//button[text()=\"Customer Login\"]"));
+        WebElement customerLoginButton = webDriver.findElement(By.xpath("//button[text()='Customer Login']"));
         customerLoginButton.click();
         WebElement yourNameField = webDriver.findElement(By.name("userSelect"));
         yourNameField.click();
-        WebElement user = webDriver.findElement(By.xpath("//*[@id=\"userSelect\"]/option[3]"));
+        WebElement user = webDriver.findElement(By.xpath("//option[text()='Harry Potter']"));
         user.click();
-        WebElement loginButton = webDriver.findElement(By.xpath("//button[text()=\"Login\"]"));
+        WebElement loginButton = webDriver.findElement(By.xpath("//button[text()='Login']"));
         loginButton.click();
-        WebElement depositButton = webDriver.findElement(By.xpath("//*[@class=\"btn btn-lg tab\"][2]"));
+        WebElement depositButton = webDriver.findElement(By.xpath("//button[contains(text(),'Deposit')]"));
         depositButton.click();
-        WebElement amountOfDepositField = webDriver.findElement(By.xpath("//input[@type=\"number\"]"));
+        WebElement amountOfDepositField = webDriver.findElement(By.xpath("//input[@type='number']"));
         amountOfDepositField.sendKeys("1000");
-        WebElement depositSubmitButton = webDriver.findElement(By.xpath("//button[@type=\"submit\"]"));
+        WebElement depositSubmitButton = webDriver.findElement(By.xpath("//button[@type='submit']"));
         depositSubmitButton.click();
-        WebElement withdrawButton = webDriver.findElement(By.xpath("//*[@class=\"btn btn-lg tab\"][2]"));
+        WebElement withdrawButton = webDriver.findElement(By.xpath("//button[contains(text(),'Withdrawl')]"));
         withdrawButton.click();
-        WebElement amountOfWithdrawField = webDriver.findElement(By.xpath("//input[@type=\"number\"]"));
+        WebElement amountOfWithdrawField = webDriver.findElement(By.xpath("//input[@type='number']"));
         amountOfWithdrawField.sendKeys("900");
-        WebElement withdrawSubmitButton = webDriver.findElement(By.xpath("//button[@type=\"submit\"]"));
+        WebElement withdrawSubmitButton = webDriver.findElement(By.xpath("//button[@type='submit']"));
         withdrawSubmitButton.click();
-        WebElement transactionButton = webDriver.findElement(By.xpath("//*[@class=\"btn btn-lg tab\"][1]"));
+        WebElement transactionButton = webDriver.findElement(By.xpath("//button[contains(text(),'Transactions')]"));
         transactionButton.click();
-        WebElement resetButton = webDriver.findElement(By.xpath("//button[text()=\"Reset\"]"));
+        WebElement resetButton = webDriver.findElement(By.xpath("//button[text()='Reset']"));
         resetButton.click();
     }
 
     @Test
     public void openAccountTab() {
         webDriver.get("https://www.globalsqa.com/angularJs-protractor/BankingProject/#/login");
-        WebElement managerLoginButton = webDriver.findElement(By.xpath("//button[text()=\"Bank Manager Login\"]"));
+        WebElement managerLoginButton = webDriver.findElement(By.xpath("//button[text()='Bank Manager Login']"));
         managerLoginButton.click();
-        WebElement addCustomerButton = webDriver.findElement(By.xpath("//*[@class=\"btn btn-lg tab\"][1]"));
+        WebElement addCustomerButton = webDriver.findElement(By.xpath("//button[contains(text(),'Add Customer')]"));
         addCustomerButton.click();
-        WebElement firstNameField = webDriver.findElement(By.xpath("//input[@placeholder=\"First Name\"]"));
+        WebElement firstNameField = webDriver.findElement(By.xpath("//input[@placeholder='First Name']"));
         firstNameField.sendKeys("Sarah");
-        WebElement lastNameField = webDriver.findElement(By.xpath("//input[@placeholder=\"Last Name\"]"));
+        WebElement lastNameField = webDriver.findElement(By.xpath("//input[@placeholder='Last Name']"));
         lastNameField.sendKeys("Connor");
-        WebElement postCodeField = webDriver.findElement(By.xpath("//input[@placeholder=\"Post Code\"]"));
+        WebElement postCodeField = webDriver.findElement(By.xpath("//input[@placeholder='Post Code']"));
         postCodeField.sendKeys("147258");
-        WebElement addCustomerButton2 = webDriver.findElement(By.xpath("//button[@type=\"submit\"]"));
+        WebElement addCustomerButton2 = webDriver.findElement(By.xpath("//button[@type='submit']"));
         addCustomerButton2.click();
         webDriver.switchTo().alert().accept();
-        WebElement openAccountButton = webDriver.findElement(By.xpath("//*[@class=\"btn btn-lg tab\"][1]"));
+        WebElement openAccountButton = webDriver.findElement(By.xpath("//button[contains(text(),'Open Account')]"));
         openAccountButton.click();
     }
 
     @Test
     public void createAccount() {
         webDriver.get("https://www.globalsqa.com/angularJs-protractor/BankingProject/#/login");
-        WebElement managerLoginButton = webDriver.findElement(By.xpath("//button[text()=\"Bank Manager Login\"]"));
+        WebElement managerLoginButton = webDriver.findElement(By.xpath("//button[text()='Bank Manager Login']"));
         managerLoginButton.click();
-        WebElement openAccountButton = webDriver.findElement(By.xpath("//*[@class=\"btn btn-lg tab\"][2]"));
+        WebElement openAccountButton = webDriver.findElement(By.xpath("//button[contains(text(),'Open Account')]"));
         openAccountButton.click();
         WebElement customerNameField = webDriver.findElement(By.name("userSelect"));
         customerNameField.click();
-        WebElement customer = webDriver.findElement(By.xpath("//*[@id=\"userSelect\"]/option[3]"));
+        WebElement customer = webDriver.findElement(By.xpath("//option[text()='Ron Weasly']"));
         customer.click();
         WebElement currencyField = webDriver.findElement(By.name("currency"));
         currencyField.click();
-        WebElement currency = webDriver.findElement(By.xpath("//*[@id=\"currency\"]/option[2]"));
+        WebElement currency = webDriver.findElement(By.xpath("//option[text()='Dollar']"));
         currency.click();
-        WebElement processButton = webDriver.findElement(By.xpath("//button[@type=\"submit\"]"));
+        WebElement processButton = webDriver.findElement(By.xpath("//button[text()='Process']"));
         processButton.click();
         webDriver.switchTo().alert().accept();
 
@@ -82,11 +82,11 @@ public class WebsiteTest extends BaseTest {
     @Test
     public void removeAllAccounts() {
         webDriver.get("https://www.globalsqa.com/angularJs-protractor/BankingProject/#/login");
-        WebElement managerLoginButton = webDriver.findElement(By.xpath("//button[text()=\"Bank Manager Login\"]"));
+        WebElement managerLoginButton = webDriver.findElement(By.xpath("//button[text()='Bank Manager Login']"));
         managerLoginButton.click();
-        WebElement customersButton = webDriver.findElement(By.xpath("//*[@class=\"btn btn-lg tab\"][3]"));
+        WebElement customersButton = webDriver.findElement(By.xpath("//button[contains(text(),'Customers')]"));
         customersButton.click();
-        List<WebElement> deleteButtons = webDriver.findElements(By.xpath("//button[text()=\"Delete\"]"));
+        List<WebElement> deleteButtons = webDriver.findElements(By.xpath("//button[text()='Delete']"));
         int size = deleteButtons.size();
         for (int i = 0; i < size; i++) {
             WebElement deleteButton = deleteButtons.get(i);
